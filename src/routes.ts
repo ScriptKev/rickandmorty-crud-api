a062403cfe749b48d8a02a2070a906df8b28e201
ScriptKev <script.kev@gmail.com>
2021-02-02 01:33:52 -0500

    some configs in the server

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,12 @@
-const routes = 'routes'
-export default routes
+import { Router, Request, Response } from 'express'
+const router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+  res.status(200).send({ message: 'Bienvenido' })
+})
+
+router.use((req: Request, res: Response) => { // Not Found
+  res.status(404).send({ message: 'Not Found' })
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
+import cors from 'cors'
 import config from '~/config'
+import apiRoutes from '~/routes'
 const server = express()
 
 server.set('port', config.PORT)
+
+server.use(cors())
+server.use(express.json())
+server.use(urlencoded({ extended: false }))
+server.use('/api', apiRoutes)
 
 export default server
